86bcbebc171e94d22a44627b5bda19b9a3183b6e
limarich <richard.esclima@gmail.com>
2026-05-23 17:06:34 -0300

    feat: add debitWithLock to IWalletRepository

--- services/wallets/src/domain/wallet/wallet.interface.ts
+++ services/wallets/src/domain/wallet/wallet.interface.ts
@@ -3,4 +3,7 @@ import { Wallet } from './wallet.entity';
 export interface IWalletRepository {
     findByPlayerId(playerId: string): Promise<Wallet | null>;
     save(wallet: Wallet): Promise<void>;
+
+    // avoid race condition
+    debitWithLock(playerId: string, amountInCents: bigint): Promise<Wallet>
 }
